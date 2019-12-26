e0a7949bf7dbc5990b41c705d3df6aa2f8cf4f89
Frederick Lee <frederick.gfl@gmail.com>
2019-12-26 17:01:58 +0000

    add package.json and base server code

--- server/server.js
+++ server/server.js
@@ -4,3 +4,37 @@
  */
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const methodOverride = require('method-override');
+
+const app = express();
+const PORT = 8080;
+
+
+app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({exteneded: true}));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['battleship', 'player1', 'player2', 'kraken', 'Kobe'],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hrs
+}));
+
+const userIdCookie = 'user_id';
+const saltRounds = 10;
+
+// app.use((req, res, next)) => {
+//   const user = getUser(re)
+
+//   next();
+// }
+
+
+//===================
+// Endpoints
+//===================
+app.get('/', (req, res) => {
+  res.render('index.html');
+})
